9f5ffe1e6400b7c9530585ece254d32b1ee70be6
Alison8547 <alisonailson100@hotmail.com>
2023-10-09 11:28:30 -0300

    feat: BiFunction

--- src/main/java/course/amigoscode/test/MyFunction.java
+++ src/main/java/course/amigoscode/test/MyFunction.java
@@ -1,5 +1,6 @@
 package course.amigoscode.test;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class MyFunction {
@@ -9,13 +10,29 @@ public class MyFunction {
         Integer integer = incrementByOneFunction.apply(4);
 
         System.out.println(integer);
+
+        Function<Integer, Integer> incrementAndMultiply = incrementByOneFunction.andThen(multiplyBy10Function);
+
+        Integer incrementMultiply = incrementAndMultiply.apply(2);
+
+        System.out.println(incrementMultiply);
+
+        System.out.println(incrementAndMultiply(2, 100));
+        System.out.println(incrementAndMultiplyFunction.apply(2, 100));
     }
 
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
 
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    static BiFunction<Integer, Integer, Integer> incrementAndMultiplyFunction = (number, numMultiply) -> (number + 1) * numMultiply;
 
     public static int incrementByOne(int number) {
         return number + 1;
+    }
+
+    public static int incrementAndMultiply(int number, int numMultiply) {
+        return (number + 1) * numMultiply;
     }
 }
