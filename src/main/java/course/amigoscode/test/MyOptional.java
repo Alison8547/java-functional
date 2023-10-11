817efd683a13dd8b804d4d964dfc2b9934f698e8
Alison8547 <alisonailson100@hotmail.com>
2023-10-11 18:02:23 -0300

    feat: Optional ifPresent and orElseThrow

--- src/main/java/course/amigoscode/test/MyOptional.java
+++ src/main/java/course/amigoscode/test/MyOptional.java
@@ -1,8 +1,21 @@
 package course.amigoscode.test;
 
+import course.amigoscode.domain.Person;
+import course.amigoscode.domain.enums.Gender;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class MyOptional {
+    private static List<Person> personList = new ArrayList<>(List.of(
+            new Person("Alison", Gender.MALE),
+            new Person("Bruna", Gender.FEMALE),
+            new Person("Saulo", Gender.MALE),
+            new Person("Rafaela", Gender.FEMALE),
+            new Person("Diego", Gender.MALE),
+            new Person("Caroline", Gender.FEMALE)));
+
     public static void main(String[] args) {
         Optional.of("hello")
                 .ifPresent(System.out::println);
@@ -20,6 +33,18 @@ public class MyOptional {
         Optional<Object> nullable = Optional.ofNullable(null);
 
         System.out.println(nullable);
+
+
+        personList.stream()
+                .filter(person -> person.getName().equals("Alison"))
+                .findFirst()
+                .ifPresent(System.out::println);
+
+
+        personList.stream()
+                .filter(person -> person.getName().equals("Chris"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Not found person!"));
 
 
     }
